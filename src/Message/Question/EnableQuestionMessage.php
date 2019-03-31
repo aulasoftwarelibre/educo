7a55859c0610b2d21bd1e27c375c15dc66cee3e0
Sergio Gómez <decano@gmail.com>
2019-03-31 18:52:05 +0200

    Enable question message skeleton

---
+++ src/Message/Question/EnableQuestionMessage.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Message\Question;
+
+
+class EnableQuestionMessage
+{
+
+}
---
+++ src/MessageHandler/Question/EnableQuestionHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\MessageHandler\Question;
+
+
+class EnableQuestionHandler
+{
+
+}
