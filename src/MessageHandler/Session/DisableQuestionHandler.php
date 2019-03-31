ffac5918eff02ba7c4aa168fcf169be4c044d647
Javi <adfjb77@gmail.com>
2019-03-31 19:41:16 +0200

    Disable question implemented

---
+++ src/MessageHandler/Session/DisableQuestionHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `edUCO` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\MessageHandler\Session;
+
+use App\Message\Session\DisableQuestionMessage;
+use App\Repository\SessionRepository;
+
+class DisableQuestionHandler
+{
+    /**
+     * @var SessionRepository
+     */
+    private $sessionRepository;
+
+    public function __construct(SessionRepository $sessionRepository)
+    {
+        $this->sessionRepository = $sessionRepository;
+    }
+
+    public function __invoke(DisableQuestionMessage $disableQuestionMessage): void
+    {
+        $id = $disableQuestionMessage->id;
+
+        $session = $this->sessionRepository->find($id);
+
+        if (!$session) {
+            throw new \InvalidArgumentException('Session not found');
+        }
+
+        $session->setIsActive(null);
+        $this->sessionRepository->save($session);
+    }
+}
