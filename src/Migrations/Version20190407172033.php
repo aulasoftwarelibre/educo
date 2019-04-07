a3aef43951a1c7b55149ce57ae269c95e6af0356
Sergio Gómez <sergio@uco.es>
2019-04-07 23:15:29 +0200

    Fixed migration down action

--- src/Migrations/Version20190407172033.php
+++ src/Migrations/Version20190407172033.php
@@ -31,7 +31,6 @@ final class Version20190407172033 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE question DROP duration');
         $this->addSql('ALTER TABLE question DROP activated_at');
     }
