30702d26e8cd532be7d911f139fb4a090b6513a1
Javi <adfjb77@gmail.com>
2019-04-07 21:06:09 +0200

    Simple crud created

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Session;
+use App\Form\SessionType;
+use App\Repository\SessionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/session")
+ */
+class SessionController extends AbstractController
+{
+    /**
+     * @Route("/", name="session_index", methods={"GET"})
+     */
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        return $this->render('session/index.html.twig', [
+            'sessions' => $sessionRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="session_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $session = new Session();
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('session_index');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'session' => $session,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="session_show", methods={"GET"})
+     */
+    public function show(Session $session): Response
+    {
+        return $this->render('session/show.html.twig', [
+            'session' => $session,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="session_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Session $session): Response
+    {
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('session_index', [
+                'id' => $session->getId(),
+            ]);
+        }
+
+        return $this->render('session/edit.html.twig', [
+            'session' => $session,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="session_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Session $session): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$session->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($session);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('session_index');
+    }
+}
---
+++ src/Form/SessionType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('isActive')
+            ->add('activeQuestion')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
