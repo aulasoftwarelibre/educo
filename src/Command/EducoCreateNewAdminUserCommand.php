da6b3a4447304f92d831f30666cdccde410e02e8
Javi <adfjb77@gmail.com>
2019-04-11 11:59:09 +0200

    Session controlled added

--- src/Command/EducoCreateNewAdminUserCommand.php
+++ src/Command/EducoCreateNewAdminUserCommand.php
@@ -1,12 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the `edUCO` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Command;
 
 use App\Message\User\CreateNewAdminUserMessage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -17,34 +27,35 @@ class EducoCreateNewAdminUserCommand extends Command
      * @var string
      */
     protected static $defaultName = 'educo:user:create:new-admin';
-
+    /**
+     * @var MessageBusInterface
+     */
     private $bus;
 
     public function __construct(MessageBusInterface $bus)
     {
         parent::__construct();
 
-
         $this->bus = $bus;
     }
 
-    protected function configure()
+    protected function configure(): void
     {
         $this
             ->setDescription('Creates a new admin user.')
             ->addArgument('username', InputArgument::REQUIRED, 'The username of the user.')
-            ->addArgument('password',  InputArgument::REQUIRED , 'User password');
-        ;
+            ->addArgument('password', InputArgument::REQUIRED, 'User password');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
         $io = new SymfonyStyle($input, $output);
         $username = $input->getArgument('username');
 
         $password = $input->getArgument('password');
-        try{
-            if(!is_string($username)) {
+
+        try {
+            if (!is_string($username)) {
                 throw new \InvalidArgumentException('Invalid username');
             }
 
@@ -54,15 +65,13 @@ class EducoCreateNewAdminUserCommand extends Command
 
             $this->bus->dispatch($message);
 
-
             $io->success('User created');
         } catch (\Throwable $e) {
             $io->error($e->getMessage());
 
             return $e->getCode();
         }
-            return 0;
+
+        return 0;
     }
-
-
 }
