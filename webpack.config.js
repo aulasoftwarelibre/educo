15a1045d48fd430f83d25439917220fe21a4e809
Javi <adfjb77@gmail.com>
2019-04-24 19:55:45 +0200

    Added Admin Template with SB Admin

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('login', './assets/js/login.js')
+    .addEntry('admin', './assets/js/admin.js')
+
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
